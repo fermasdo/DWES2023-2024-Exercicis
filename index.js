35981fc0f9369042a3cbbe52a6050eaa39b9ebea
Ferran Mas Doménech <Ferran.Mas@iestacio.com>
2023-09-28 13:34:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const os = require('os');
+
+let app = express();
+
+app.get('/benvinguda', (req, res) => {
+    res.send('Hola, benvingut/a');
+});
+
+app.get('/data', (req, res) => {
+     
+    res.send(new Date() );
+});
+
+app.get('/usuari', (req, res) => {
+    res.send(os.userInfo().username);
+});
+
+
+app.listen(8080);
