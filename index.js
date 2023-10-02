9ba4c37d6a03e42704554a04aeeb6b6779bd0ead
Ferran Mas Doménech <Ferran.Mas@iestacio.com>
2023-10-02 22:55:28 +0200

    Solució Exercici4 Express

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const app = express()
 
 const PORT = 3000
 
+// Middleware per enregistrar peticions
+app.use((req,res,next)=>{
+  console.log('Rebuda una petició ', req.ip ,' a les', new Date().toLocaleString())
+  console.log('Tipus ', req.method)
+  console.log('URL ', req.protocol,'://',req.headers.host , req.url)
+  next()
+})
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/un', (req, res) => {
+  res.send('Un')
+})
+
+app.get('/dos', (req, res) => {
+  res.send('Dos')
+})
+
+// Gestionar 404 Not Found
+app.use((req,res)=>{
+  res.status(404).send('404 - Not Found')
 })
 
 app.listen(PORT, () => {
-  console.log(`Exercici 3 - App is running on http://localhost:${PORT}`)
+  console.log(`Exercici - App is running on http://localhost:${PORT}`)
 })
