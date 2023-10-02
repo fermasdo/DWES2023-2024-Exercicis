3c59fe96c6c535cd5cf69c8f2d9ab3e7d339435b
Ferran Mas Doménech <Ferran.Mas@iestacio.com>
2023-10-02 23:20:23 +0200

    Solució Exercici 5 Express

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = 3000
+// Utilitzar express.json() com a middleware per analitzar dades JSON
+app.use(express.json());
 
+// Ruta POST que rep dades JSON
+app.post('/api/dades', (req, res) => {
+  const dades = req.body; // Aquí pots accedir a les dades JSON enviades en la sol·licitud
+  
+  // Totes les dades rebudes
+  console.log(dades);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  // Per accedir a un camp en concret pots fer-ho així:
+  console.log('Nom: ', dades.usuari.nom);
+  res.status(200).send('Dades rebudes amb èxit');
+});
 
-app.listen(PORT, () => {
-  console.log(`Exercici 3 - App is running on http://localhost:${PORT}`)
-})
+app.listen(3000, () => {
+  console.log('Servidor Express escoltant al port 3000');
+});
